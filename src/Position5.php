b77110fafba1c9c254f0f5520684b87a44abc745
BenjaminKrammel <benjaminkrammel@internetx.de>
2018-08-24 15:24:22 +0200

    initial

---
+++ src/Position5.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Position5;
+
+/**
+ *
+ */
+class Position5
+{
+    public static function getFromArray($arr, $key, $default = '')
+    {
+        if (!empty($validatedData['fileMode'])) {
+            return $validatedData['fileMode'];
+        }
+        return $default;
+    }
+}
